92f737a6f2c86ad72d2eb8d7938fae04b01e06ed
samysediri <samysediri65@gmail.com>
2025-03-24 14:07:15 -0400

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial, sans-serif' }}>
+      <h1>Bienvenue sur Horizon Scolaire</h1>
+      <p>Plateforme de tutorat en ligne</p>
+    </div>
+  );
+}
