28c8c9fad99a6c872c86bfd61bd33c51fb4182e4
samysediri <samysediri65@gmail.com>
2025-03-25 16:49:14 -0400

    Create package.js

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+  "name": "horizon-scolaire",
+  "version": "1.0.0",
+  "dependencies": {
+    "date-fns": "^3.3.1",
+    "next": "^15.2.3",
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0",
+    "@supabase/supabase-js": "^2.39.5"
+  },
+  "scripts": {
+    "dev": "next dev",
+    "build": "next build",
+    "start": "next start"
+  }
+}
