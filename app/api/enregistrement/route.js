d16c6d2f816580c7760547b0a90e6ff7ffe63359
samysediri <samysediri65@gmail.com>
2025-03-27 13:26:10 -0400

    Delete app/api/enregistrement/route.js

--- app/api/enregistrement/route.js
+++
@@ -1,6 +0,0 @@
-export async function GET() {
-  return new Response(JSON.stringify({ message: 'API fonctionnelle!' }), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' }
-  })
-}
