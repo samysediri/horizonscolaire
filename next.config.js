7032ec56b38bfe99cc758333d81c5796e54acdad
samysediri <samysediri65@gmail.com>
2025-03-29 15:30:53 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 // next.config.js
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverActions: true,
+    appDir: true,
+  }
 }
 
 module.exports = nextConfig
-
-
